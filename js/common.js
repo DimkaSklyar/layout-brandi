c212bde202cc0e689eb9395a74e1529f6aa59ce3
DimkaSklyar <DimkaSklyar@mail.ru>
2018-03-13 22:09:18 +0300

    section-2

--- js/common.js
+++ js/common.js
@@ -1,19 +1,30 @@
 $(document).ready(function() {
-	var amount = $('.slider-item').length;
-	var number = 0;
 
-setInterval(function(){
-  $('.slider-item')
-		.removeAttr('style')
-		.removeClass('slider-item-active');
+	var amount = $('.slider-item').length;
+	var number = 1;
+	var tick = 3500;
+
+var slider = setInterval(function(){
+  $('.slider-item').removeClass('slider-item-active');
 	if (number == amount ) {
   	number = 0;
+		$('.slider-item:not(:nth-of-type(' + amount + '))').removeAttr('style');
+		$('.slider-item:nth-of-type(' + amount + ')').css({zIndex: 0});
   }
   number++;
-  	$('.slider-item:nth-of-type('+ number + ')').css({ 
-		backgroundImage: 'url(../img/header-' + number +'.jpg', zIndex: number}).addClass('slider-item-active active');
+  	$('.slider-item:nth-of-type('+ number + ')')
+			.css({
+				backgroundImage: 'url(../img/header-' + number +'.jpg',
+				zIndex: number
+			})
+			.addClass('slider-item-active');
+		$('.slider-item:nth-of-type('+ (number - 1) + ')').css({zIndex: number-1});
 		$('#nav-slider-' + number).prop('checked',true);
-		$('.slider-item').removeClass('active');
-}, 2500);
+}, tick);
+
+//stop
+$('label[for="nav-slider-1"]').click(function(){
+	clearInterval(slider);
+});
 
 });
